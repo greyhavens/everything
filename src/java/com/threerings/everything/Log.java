d463d4a3cb4738cec68ec21938f416f70d1ef51d
Michael Bayne <mdb@threerings.net>
2009-07-04 03:04:42 +0000

    A skeleton for Everything to hang off of.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@2 a9e240f3-4568-de11-90a4-003048321218

---
+++ src/java/com/threerings/everything/Log.java
@@ -0,0 +1,15 @@
+//
+// $Id$
+
+package com.threerings.everything;
+
+import com.samskivert.util.Logger;
+
+/**
+ * Contains a reference to the log object used by this app.
+ */
+public class Log
+{
+    /** The static log instance configured for use by this app. */
+    public static Logger log = Logger.getLogger("com.threerings.everything");
+}
---
+++ src/java/com/threerings/everything/server/EverythingApp.java
@@ -0,0 +1,73 @@
+//
+// $Id$
+
+package com.threerings.everything.server;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+
+import com.samskivert.util.Config;
+
+import com.threerings.samsara.app.data.AppCodes;
+import com.threerings.samsara.app.server.App;
+import com.threerings.samsara.app.server.Binding;
+
+import static com.threerings.everything.Log.log;
+
+/**
+ * The main entry point for the Everything app.
+ */
+public class EverythingApp extends App
+{
+    /** Our app identifier. */
+    public static final String IDENT = "everything";
+
+    public static class Module extends AbstractModule
+    {
+        @Override protected void configure () {
+            bind(App.class).to(EverythingApp.class);
+        }
+    }
+
+    @Override // from App
+    public String getIdentifier ()
+    {
+        return IDENT;
+    }
+
+    @Override // from App
+    public Binding[] getBindings ()
+    {
+        List<Binding> bindings = Lists.newArrayList();
+        // bindings.add(new Binding.Servlet("/" + app.trim() + "/", EverythingServlet.class));
+        return bindings.toArray(new Binding[bindings.size()]);
+    }
+
+    @Override // from App
+    public String getFacebookSecret (String uri)
+    {
+        return null; // TODO
+    }
+
+    @Override // from App
+    public void didInit ()
+    {
+        log.info("Everything app initialized.", "version", _appvers);
+    }
+
+    @Override // from App
+    public void didDetach ()
+    {
+        log.info("Everything app detached.", "version", _appvers);
+    }
+
+    protected Config _config = new Config("everything", getClass().getClassLoader());
+
+    @Inject protected @Named(AppCodes.APPVERS) String _appvers;
+}
