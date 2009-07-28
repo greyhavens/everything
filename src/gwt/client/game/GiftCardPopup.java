adb336e20b3d997de1d32d85de9ca061c5f3e264
Michael Bayne <mdb@threerings.net>
2009-07-28 05:49:31 +0000

    Need a History import here.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@315 a9e240f3-4568-de11-90a4-003048321218

--- src/gwt/client/game/GiftCardPopup.java
+++ src/gwt/client/game/GiftCardPopup.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.PushButton;
