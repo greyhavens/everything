6d2f24c55582322e01e93436972744ffd309c0ee
Michael Bayne <mdb@threerings.net>
2009-08-07 23:37:46 +0000

    Simplified the new player information. Four steps to Everything bliss in large font and using simple words.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@409 a9e240f3-4568-de11-90a4-003048321218

--- src/gwt/client/game/AddAppPanel.java
+++ src/gwt/client/game/AddAppPanel.java
@@ -3,10 +3,15 @@
 
 package client.game;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
+
+import com.threerings.gwt.ui.SmartTable;
 import com.threerings.gwt.ui.Widgets;
 
+import client.util.Args;
 import client.util.Context;
+import client.util.Page;
 
 /**
  * Displays a request to add our app.
@@ -19,7 +24,28 @@ public class AddAppPanel extends FlowPanel
         if (asPage) {
             addStyleName("page");
         }
-        add(Widgets.newHTML("Click " + ctx.getFacebookAddLink("Add Everything") +
-                            " to add the Everything app and start playing!", "machine"));
+
+        add(Widgets.newLabel(_msgs.introTitle(), "Title", "machine"));
+        add(Widgets.newLabel(_msgs.introIntro(), "Text"));
+        int col = 0;
+        SmartTable intro = new SmartTable("Steps", 5, 0);
+        intro.setText(0, col++, _msgs.introStepOne(), 1, "machine");
+        intro.setText(0, col++, "\u2023", 1, "Arrow");
+        intro.setText(0, col++, _msgs.introStepTwo(), 1, "machine");
+        intro.setText(0, col++, "\u2023", 1, "Arrow");
+        intro.setText(0, col++, _msgs.introStepThree(), 1, "machine");
+        intro.setText(0, col++, "\u2023", 1, "Arrow");
+        intro.setText(0, col++, _msgs.introStepFour(), 1, "machine");
+        add(intro);
+
+        if (ctx.getMe().isGuest()) {
+            add(Widgets.newHTML("Click " + ctx.getFacebookAddLink("Add Everything") +
+                                " to add the Everything app and start playing!", "machine"));
+        } else {
+            add(Widgets.newFlowPanel("Text", Widgets.newInlineLabel(_msgs.introReady() + " "),
+                                     Args.createInlink(_msgs.introFlip(), Page.FLIP)));
+        }
     }
+
+    protected static final GameMessages _msgs = GWT.create(GameMessages.class);
 }
--- src/gwt/client/game/LandingPage.java
+++ src/gwt/client/game/LandingPage.java
@@ -3,8 +3,10 @@
 
 package client.game;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
 
+import com.threerings.gwt.ui.SmartTable;
 import com.threerings.gwt.ui.Widgets;
 import com.threerings.gwt.util.DateUtil;
 import com.threerings.gwt.util.Value;
@@ -24,20 +26,9 @@ public class LandingPage extends FlowPanel
         setStyleName("landing");
         addStyleName("page"); // we're a top-level page
 
-        if (ctx.getMe().isGuest()) {
+        if (ctx.getMe().isGuest() || ctx.isNewbie()) {
             add(new AddAppPanel(ctx, false));
-        }
-
-        if (ctx.isNewbie()) {
-            add(Widgets.newLabel("Welcome to Everything!", "Title"));
-            FlowPanel intro = new FlowPanel();
-            for (String text : INTRO_HTML) {
-                intro.add(Widgets.newHTML(text, "Text"));
-            }
-            add(intro);
-        }
-
-        if (news.get() == null) {
+        } else if (news.get() == null) {
             add(Widgets.newLabel("Latest News", "Title"));
             add(Widgets.newLabel("No gnus is good gnus.", "Text"));
         } else {
@@ -45,12 +36,15 @@ public class LandingPage extends FlowPanel
             add(Widgets.newHTML(formatNews(news.get().text), "Text"));
         }
 
+        add(Widgets.newShim(15, 15));
         if (!ctx.getMe().isGuest()) {
             add(Widgets.newLabel("Recent Happenings", "Title"));
             add(new FeedPanel(ctx));
         }
 
-        add(Widgets.newLabel("Build: " + Build.time(), "machine"));
+        if (ctx.isEditor()) {
+            add(Widgets.newLabel("Build: " + Build.time(), "machine"));
+        }
     }
 
     protected static String formatNews (String text)
@@ -78,14 +72,5 @@ public class LandingPage extends FlowPanel
         return buf.toString();
     }
 
-    protected static final String[] INTRO_HTML = {
-        "Everything is a collecting game. Every day you get a new grid of cards and you " +
-        "flip them over to get new things to add to your collection. Some cards are rarer " +
-        "than others, so good luck.",
-        "You get five free flips every day, but you can buy more flips by spending coins " +
-        "you get from cashing in cards you don't want. Rarer cards are worth more coins when " +
-        "you cash them in. You can also give cards to your friends to help them complete their " +
-        "collections.",
-        "Enough jabber, click <b>Flip Cards</b> up above to get started!"
-    };
+    protected static final GameMessages _msgs = GWT.create(GameMessages.class);
 }
