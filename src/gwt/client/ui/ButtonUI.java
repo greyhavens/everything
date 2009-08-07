adc84b82a818ed255eb1c9f5cec6808367d971d9
Michael Bayne <mdb@threerings.net>
2009-08-07 23:47:54 +0000

    We need to set the style rather than add it to get the different modes working.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@410 a9e240f3-4568-de11-90a4-003048321218

--- src/gwt/client/ui/ButtonUI.java
+++ src/gwt/client/ui/ButtonUI.java
@@ -27,10 +27,9 @@ public class ButtonUI
     public static PushButton newButton (String text, ClickHandler onClick)
     {
         PushButton button = new PushButton(text);
+        button.setStyleName("everyButton");
         if (text.length() > 5) {
             button.addStyleName("longButton");
-        } else {
-            button.addStyleName("normalButton");
         }
         if (onClick != null) {
             button.addClickHandler(onClick);
