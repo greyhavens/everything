efdf48d685dec4f2b3e6e4a7fba80c9825c7788a
Ray Greenwell <ray@threerings.net>
2009-11-20 03:09:53 +0000

    Rejigger to go back to using "streamPublish", which is soon going to be the only thing supported by Facebook. Looks ok at first blush, but my images aren't showing up. I'll play with the candidate on the production servers.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@676 a9e240f3-4568-de11-90a4-003048321218

--- src/gwt/client/game/ThingDialog.java
+++ src/gwt/client/game/ThingDialog.java
@@ -33,13 +33,13 @@ public class ThingDialog
     public static ClickHandler makeGotHandler (Context ctx, Card card, boolean completed)
     {
         if (completed) {
-            return makeHandler(ctx, "got_comp", Build.Template.GOT_COMP.id, card,
+            return makeHandler(ctx, "got_comp", card,
                                "Celebrate your completed series:", "Woo hoo!");
         } else if (card.giver != null) {
-            return makeHandler(ctx, "got_gift", Build.Template.GOT_GIFT.id, card,
+            return makeHandler(ctx, "got_gift", card,
                                "Thank your friend for this great gift:", "Thanks!");
         } else {
-            return makeHandler(ctx, "got_card", Build.Template.GOT_CARD.id, card,
+            return makeHandler(ctx, "got_card", card,
                                "Tell your friends about this awesome card:", "Woo!");
         }
     }
@@ -49,7 +49,7 @@ public class ThingDialog
      */
     public static ClickHandler makeWantHandler (Context ctx, Card card)
     {
-        return makeHandler(ctx, "want_card", Build.Template.WANT_CARD.id, card,
+        return makeHandler(ctx, "want_card", card,
                            "Let your friends know you want this card:", "I wants it!");
     }
 
@@ -58,23 +58,23 @@ public class ThingDialog
      */
     public static void showGifted (Context ctx, Card card, PlayerName target)
     {
-        showDialog(ctx, "gave_card", Build.Template.GAVE_CARD.id, ""+target.facebookId, card,
+        showDialog(ctx, "gave_card", ""+target.facebookId, card,
                    target, "Tell your friends why you gave " + target.name + " this card:", "");
     }
 
     protected static ClickHandler makeHandler (final Context ctx, final String vec,
-                                               final String templateId, final Card card,
-                                               final String prompt, final String message)
+                                               final Card card, final String prompt,
+                                               final String message)
     {
         return new ClickHandler() {
             public void onClick (ClickEvent event) {
                 String targetId = (card.giver == null) ? null : (""+card.giver.facebookId);
-                showDialog(ctx, vec, templateId, targetId, card, card.owner, prompt, message);
+                showDialog(ctx, vec, targetId, card, card.owner, prompt, message);
             }
         };
     }
 
-    protected static void showDialog (Context ctx, String vec, String templateId, String targetId,
+    protected static void showDialog (Context ctx, String vec, String targetId,
                                       Card card, PlayerName owner, String prompt, String message)
     {
         final String tracking = KontagentUtil.generateUniqueId(ctx.getMe().userId);
@@ -82,7 +82,7 @@ public class ThingDialog
             Kontagent.POST, tracking, Page.BROWSE, owner.userId, card.thing.categoryId);
         String everyURL = ctx.getEverythingURL(Kontagent.POST, tracking, Page.LANDING);
         String imageURL = GWT.getModuleBaseURL() + "cardimg?thing=" + card.thing.thingId;
-        showDialog(templateId, targetId, card.thing.name, card.thing.descrip,
+        showDialog(targetId, card.thing.name, card.thing.descrip,
                    Category.getHierarchy(card.categories), card.getSeries().name,
                    card.thing.rarity.toString(), imageURL,
                    cardURL, everyURL, prompt, message, new Command() {
@@ -97,55 +97,34 @@ public class ThingDialog
         });
     }
 
-    protected static native void showDialog (String templateId, String targetId, String thing,
-                                             String descrip, String categories, String series,
-                                             String rarity, String image, String cardURL,
-                                             String everyURL, String prompt, String message,
+    protected static native void showDialog (String targetId, String thing, String descrip,
+                                             String categories, String series, String rarity,
+                                             String imageURL, String cardURL, String everyURL,
+                                             String prompt, String message,
                                              Command onComplete) /*-{
-        var data = {
-            'thing': thing,
-            'descrip': descrip,
-            'category': categories,
-            'series': series,
-            'rarity': rarity,
-            'cardURL': cardURL,
-            'everyURL': everyURL,
-            'images': [{ 'src': image, 'href': cardURL }],
+        var attachment = {
+            'name': thing,
+            'href': everyURL,
+            'description': descrip,
+            'media': [{ 'type': 'image',
+                        'src': imageURL,
+                        'href': cardURL }],
+            'properties': {'category': categories,
+                           'series': series,
+                           'rarity': rarity,
+                           'Join the fun': {'text': 'Play Everything!',
+                                            'href': everyURL }},
         };
-        var target = (targetId == null) ? null : [ targetId ];
-        var msg = { value: message };
-        $wnd.FB.Connect.showFeedDialog(templateId, data, target, null, null, null, function () {
-            // we used to try to send msg.value to this method but Facebook doesn't actually update
-            // it with the changed text, at least not in any of the numerous tests I did; awesome!
-            // not that it would matter because we still don't know for sure one way or the other
-            // if the user clicked Post or Skip because Facebook won't tell us that information,
-            // because we "can't be trusted with it"
-            onComplete.@com.google.gwt.user.client.Command::execute()();
-        }, prompt, msg);
+        var actions = [{ "text": "Play Everything",
+                         "href": everyURL }];
+        $wnd.FB.Connect.streamPublish(message, attachment, actions, targetId, prompt,
+            function (postId, exception) {
+                if (postId != null) {
+                    onComplete.@com.google.gwt.user.client.Command::execute()();
+                }
+            }
+        );
     }-*/;
-
-// We used to use streamPublish, which is *way* simpler but does not allow us to highlight text in
-// the title which is critical for making our feed stories not look like random news articles
-//
-//     protected static native void showDialog (String message, String thing, String descrip,
-//                                              String category, String rarity, String image,
-//                                              String url) /*-{
-//         var attachment = {
-//             'name': thing,
-//             'href': url,
-//             'description': descrip,
-//             'media': [{ 'type': 'image',
-//                         'src': image,
-//                         'href': url }],
-//             'properties': {'Category': category,
-//                            'Rarity': rarity,
-//                            'Join the fun': {'text': 'Play Everything!',
-//                                             'href': 'http://apps.facebook.com/everythinggame/'}},
-//         };
-//         var actions = [{ "text": "Play Everything",
-//                          "href": "http://apps.facebook.com/everythinggame/"}];
-//         $wnd.FB.Connect.streamPublish(message, attachment, actions);
-//     }-*/;
 
     protected static final EverythingServiceAsync _everysvc = GWT.create(EverythingService.class);
 }
