af1fa016a373830bd484d94a67a68d5795c55070
Ray Greenwell <ray@threerings.net>
2009-12-04 01:31:08 +0000

    Argh. Might as well checkpoint this before I redo things again. - A new popup that is more distinct, we need to explain what's   going on when they get the bonanza.   - Much love needed.   - Presently you can't examine the card more closely, but could do. - Tried to make the card show in a new slot behind the flipped card,   but that's been a non-starter.. maybe I can restyle the card dealies   to make them non-transparent, but I also need to change indexes...   Or I might be able to just adjust all the cards on that row, and   really animate how there's suddenly an extra card.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@695 a9e240f3-4568-de11-90a4-003048321218

---
+++ src/gwt/client/game/BonanzaPopup.java
@@ -0,0 +1,118 @@
+//
+// $Id$
+
+package client.game;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.Widget;
+
+import com.threerings.gwt.ui.FX;
+import com.threerings.gwt.ui.FluentTable;
+import com.threerings.gwt.ui.Widgets;
+
+import com.threerings.everything.client.GameService;
+import com.threerings.everything.client.GameServiceAsync;
+import com.threerings.everything.data.GameStatus;
+import com.threerings.everything.data.SlotStatus;
+
+import client.ui.ButtonUI;
+import client.util.ClickCallback;
+import client.util.Context;
+
+public class BonanzaPopup extends PopupPanel
+{
+    public BonanzaPopup (
+        Context ctx, final GameService.BonanzaInfo bonanzaInfo,
+        final AsyncCallback<GameStatus> callback)
+    {
+        setStyleName("popup");
+        _ctx = ctx;
+
+        FluentTable box = new FluentTable(0, 0, "bonanza");
+        box.add().alignCenter().setHTML("BONANZA");
+        box.add().alignCenter().setHTML(bonanzaInfo.title);
+        box.add().setHTML(
+            "You found an extra card. It wouldn't be fair for you to keep it, but if you post " +
+            "it to your feed, we'll grant you an extra free flip for your efforts.");
+
+        SlotView slot = new SlotView();
+        slot.setStatus(SlotStatus.FLIPPED);
+        slot.setCard(ctx, bonanzaInfo.card.toThingCard(), false, new ClickHandler() {
+            public void onClick (ClickEvent event) {
+                // nada
+            }
+        });
+        box.add().setWidgets(slot, Widgets.newHTML(bonanzaInfo.message));
+
+        PushButton post = ButtonUI.newButton("Post", new ClickHandler() {
+            public void onClick (ClickEvent event) {
+                ThingDialog.showAttractor(_ctx, bonanzaInfo.card, bonanzaInfo.title,
+                    bonanzaInfo.message, new AsyncCallback<Boolean>() {
+                        public void onSuccess (Boolean posted) {
+                            if (!posted) {
+                                return; // just leave the dialog up
+                            }
+                            _gamesvc.bonanzaViewed(true, new AsyncCallback<GameStatus>() {
+                                public void onSuccess (GameStatus status) {
+                                    callback.onSuccess(status);
+                                }
+
+                                public void onFailure (Throwable cause) {
+                                    // TODO (shouldn't happen)
+                                }
+                            });
+                            onHide().onClick(null);
+                        }
+                        public void onFailure (Throwable cause) {
+                            // TODO (shouldn't happen)
+                        }
+                    });
+            }
+        });
+        PushButton cancel = ButtonUI.newButton("Skip", onHide());
+        new ClickCallback<GameStatus>(cancel) {
+            protected boolean callService () {
+                _gamesvc.bonanzaViewed(false, this);
+                return true;
+            }
+            protected boolean gotResult (GameStatus status) {
+                // status == null, don't do shit.. right?
+                return true;
+            }
+        };
+
+        box.add().setWidget(Widgets.newRow(cancel, post));
+
+        setWidget(box);
+
+        // now animate it in
+        setVisible(false);
+        _ctx.displayPopup(this, null);
+        FX.move(this).from(-getOffsetWidth(), getAbsoluteTop()).run(500);
+    }
+
+    protected ClickHandler onHide ()
+    {
+        return new ClickHandler() {
+            public void onClick (ClickEvent event) {
+                int tx = -getOffsetWidth(), ty = getAbsoluteTop();
+                FX.move(BonanzaPopup.this).to(tx, ty).onComplete(new Command() {
+                    public void execute () {
+                        hide();
+                    }
+                }).run(500);
+            }
+        };
+    }
+
+    protected Context _ctx;
+
+    protected static final GameServiceAsync _gamesvc = GWT.create(GameService.class);
+}
--- src/gwt/client/game/FlipPage.java
+++ src/gwt/client/game/FlipPage.java
@@ -244,22 +244,49 @@ public class FlipPage extends DataPanel<GameService.GridResult>
                 // display the flipped card in the grid
                 _slots[position].setCard(_ctx, result.card.toThingCard(), false, null);
 
+                if (result.bonanza != null) {
+                    // TODO: work out how to display the bonanza card on the grid...
+                    // 1) A little behind and shifted on the card you clicked.
+                    // 2) Squeeze it in. Shift the whole row around so that it's crowded.
+//                    _bonanza = new SlotView();
+//                    _bonanza.setStatus(SlotStatus.FLIPPED);
+//                    _bonanza.setCard(_ctx, result.bonanza.card.toThingCard(), false,
+//                        new ClickHandler() {
+//                            public void onClick (ClickEvent event) {
+//                                // TODO: change it so that they can react the bonanza at any time,
+//                                // thus granting the free flip.
+//                                new BonanzaPopup(_ctx, result.bonanza,
+//                                    new AsyncCallback<GameStatus>() {
+//                                    public void onSuccess (GameStatus status) {
+//                                        updateGameStatus(_data.status = status);
+//                                    }
+//                                    public void onFailure (Throwable caught) {
+//                                        // nada
+//                                    }
+//                                });
+//                            }
+//                        });
+//                    _cards.add(_bonanza, _cards.getWidgetLeft(_slots[position]) + 10,
+//                        _cards.getWidgetTop(_slots[position]));
+                    // TODO: mutherfuck! We need to put the bonanza card "behind"
+                    //_cards.adjustIndex(_bonanza, 0);
+                }
+
                 // display the card big and fancy and allow them to gift it or cash it in
                 CardPopup popup = CardPopup.display(
                     _ctx, result, _slots[position].status, _slots[position], null);
                 if (result.bonanza != null) {
                     popup.addCloseHandler(new CloseHandler<PopupPanel>() {
                         public void onClose (CloseEvent<PopupPanel> event) {
-                            // BONANZA!!!!
-                            CardPopup.displayBonanza(_ctx, result.bonanza,
+                            new BonanzaPopup(_ctx, result.bonanza,
                                 new AsyncCallback<GameStatus>() {
-                                    public void onSuccess (GameStatus status) {
-                                        updateGameStatus(_data.status = status);
-                                    }
-                                    public void onFailure (Throwable caught) {
-                                        // nada
-                                    }
-                                });
+                                public void onSuccess (GameStatus status) {
+                                    updateGameStatus(_data.status = status);
+                                }
+                                public void onFailure (Throwable caught) {
+                                    // nada
+                                }
+                            });
                         }
                     });
                 }
@@ -390,6 +417,9 @@ public class FlipPage extends DataPanel<GameService.GridResult>
 
     protected AbsolutePanel _cards;
     protected SlotView[] _slots;
+
+    /** Our bonanza card, if any. */
+    protected SlotView _bonanza;
 
     protected static final GameServiceAsync _gamesvc = GWT.create(GameService.class);
 }
