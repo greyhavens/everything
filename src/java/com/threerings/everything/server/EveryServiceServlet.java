41efe80d52f9ee21eb8dbac11513a778b627a63d
Michael Bayne <mdb@threerings.net>
2011-03-15 00:38:55 +0000

    Unexpected error reporting is now handled by AppServiceServlet.
    git-svn-id: svn+ssh://src.earth.threerings.net/everything/trunk@927 a9e240f3-4568-de11-90a4-003048321218

--- src/java/com/threerings/everything/server/EveryServiceServlet.java
+++ src/java/com/threerings/everything/server/EveryServiceServlet.java
@@ -3,13 +3,7 @@
 
 package com.threerings.everything.server;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.gwt.user.client.rpc.SerializationException;
-import com.google.gwt.user.server.rpc.UnexpectedException;
 import com.google.inject.Inject;
 
 import com.threerings.user.OOOUser;
@@ -36,47 +30,6 @@ public abstract class EveryServiceServlet extends AppServiceServlet
             return super.processCall(payload);
         } finally {
             _perThreadPlayerRecord.remove();
-        }
-    }
-
-    @Override // TODO: move this into AppServiceServlet
-    protected void doUnexpectedFailure (Throwable error)
-    {
-        HttpServletRequest req = getThreadLocalRequest();
-        HttpServletResponse rsp = getThreadLocalResponse();
-
-        // if this is an "unexpected exception", unwrap the inner exception
-        if (error instanceof UnexpectedException) {
-            error = error.getCause();
-        }
-
-        // log the failure to the application log
-        log.warning("Service request failure", "uri", req.getRequestURI(), error);
-
-        // in case we failed while writing the response, we need to reset the response (from
-        // RemoteServiceServlet)
-        try {
-            rsp.reset();
-        } catch (IllegalStateException ex) {
-            // If we can't reset the request, the only way to signal that something has gone wrong
-            // is to throw an exception from here. It should be the case that we call the user's
-            // implementation code before emitting data into the response, so the only time that
-            // gets tripped is if the object serialization code blows up.
-            throw new RuntimeException("Unable to report failure", error);
-        }
-
-        // send a standard failure response
-        try {
-            rsp.setContentType("text/plain");
-            rsp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            try {
-                rsp.getOutputStream().write(GENERIC_FAILURE_MSG.getBytes("UTF-8"));
-            } catch (IllegalStateException ex) {
-                // Handle the (unexpected) case where getWriter() was previously used
-                rsp.getWriter().write(GENERIC_FAILURE_MSG);
-            }
-        } catch (IOException ioe) {
-            log.warning("Failed to write failure response", ioe);
         }
     }
 
@@ -120,8 +73,4 @@ public abstract class EveryServiceServlet extends AppServiceServlet
 
     @Inject protected EverythingApp _app;
     @Inject protected PlayerRepository _playerRepo;
-
-    // used by doUnexpectedFailure
-    protected static final String GENERIC_FAILURE_MSG =
-        "The call failed on the server; see server log for details";
 }
