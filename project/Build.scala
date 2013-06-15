1861d3da14aadac8d9ecc48c7dafade8d074b472
Michael Bayne <mdb@samskivert.com>
2013-06-14 17:00:27 -0700

    Added simple SBT build back. Mainly for shared.

---
+++ project/Build.scala
@@ -0,0 +1,16 @@
+import sbt._
+import sbt.Keys._
+
+object EverythingBuild extends samskivert.MavenBuild {
+
+  override val globalSettings = Seq(
+    crossPaths      := false,
+    scalacOptions   ++= Seq("-unchecked", "-deprecation", "-feature",
+                            "-language:implicitConversions"),
+    fork in Compile := true,
+    libraryDependencies ++= Seq(
+      "com.novocode" % "junit-interface" % "0.7" % "test->default" // make JUnit tests work
+    ),
+    resolvers += "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository"
+  )
+}
